5c154024506df29e1a01a68f7265318f7f8b2537
JMJcodes <134827059+JMJcodes@users.noreply.github.com>
2025-01-04 20:57:46 -0800

    Update script.js
    Spruced up the text

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const ctx = canvas.getContext("2d");
 let word = "";
 let topic = "";
 let wordBank = {
-  fruits: ["apple", "banana", "cherry"],
-  vegetables: ["carrot", "onion", "pepper"],
-  desserts: ["cake", "cookie", "pudding"],
-  animals: ["elephant", "giraffe", "monkey"],
-  colors: ["red", "blue", "green"],
+  Fruits: ["Apple", "Grape", "Banana", "Blueberry", "Strawberry", "Fig", "Pear", "Avocado", "Cherry", "Plum", "Pineapple", "Tangelo", "Watermelon", "Canteloupe", "Lemon", "Lime", "Papaya", "Raspberry", "Apricot", "Blackberry", "Cranberry", "Oranges"],
+  Vegetables: ["Carrot", "Potato", "Spinach", "Onion", "Broccoli", "Beet", "Lettuce", "Parsley", "Spinach", "Chives", "Broccoli", "Artichoke", "Garlic", "Celery", "Rhubarb", "Peas", "Eggplant", "Cucumber", "Tomato", "Pepper", "Kale"],
+  Desserts: ["Icecream", "Cookie", "Pudding", "Cake", "Popsicle", "Cupcake", "Brownie", "Pie", "Cheesecake", "Cobbler", "Chocolate", "Candy"], 
+  Animals: ["Elephant", "Giraffe", "Kangaroo", "Zebra", "Penguin", "Goat", "Parrot", "Tucan", "Pig", "Goose", "Chicken", "Fish", "Monkey", "Rat", "Owl", "Bear", "Gorilla", "Cow", "Dog", "Donkey", "Horse", "Lion", "Deer", "Yak", "Fox", "Raccoon", "Rabbit", "Cat", "Sheep", "Sloth", "Koala", "Wolf", "Coyote", "Jaguar", "Hippopotamus", "Llama", "Alligator", "Crocodile", "Whale", "Dolphin", "Snake", "Squirrel", "Frog", "Toad", "Gopher", "Mole", "Hedgehog", "Otter", "Reindeer", "Porcupine", "Leopard", "Cheetah", "Panda", "Rhinoceros", "Bat"],
+  Colors: ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Purple", "Pink", "White", "Black"]
 };
 let guessedLetters = [];
 let incorrectGuesses = 0;
